7792ff7d10da2ec089d0f3d6b187889812a478fd
Ben Werner <bewerner23@gmail.com>
2026-05-12 21:31:33 -0700

    Prepare Rigkit v0.2.0

--- packages/cli/src/version.ts
+++ packages/cli/src/version.ts
@@ -1,1 +1,1 @@
-export const RIGKIT_CLI_VERSION = "0.1.9";
+export const RIGKIT_CLI_VERSION = "0.2.0";
--- packages/provider-cmux/src/version.ts
+++ packages/provider-cmux/src/version.ts
@@ -1,1 +1,1 @@
-export const RIGKIT_PROVIDER_CMUX_VERSION = "0.1.9";
+export const RIGKIT_PROVIDER_CMUX_VERSION = "0.2.0";
